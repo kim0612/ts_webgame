ca1451a61362a4895491a3b0996c3f7dd9f46c26
kim0612 <roki03@naver.com>
2021-10-06 19:15:59 +0900

    v1_gugudan

--- lecture/lecture.js
+++ lecture/lecture.js
@@ -1,2 +1,33 @@
-var numberOne = 192;
-console.log(numberOne * 9);
+var numOne = Math.ceil(Math.random() * 9);
+var numTwo = Math.ceil(Math.random() * 9);
+var result = numOne * numTwo;
+var word = document.createElement('div');
+word.textContent = numOne + " multiple " + numTwo + " = ?";
+document.body.append(word);
+var form = document.createElement('form');
+document.body.append(form);
+var input = document.createElement('input');
+form.append(input);
+var button = document.createElement('button');
+button.type = 'submit';
+button.textContent = '입력!';
+form.append(button);
+var resultDiv = document.createElement('div');
+document.body.append(resultDiv);
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (result === Number(input.value)) { //정답 맞춘경우
+        resultDiv.innerText = '정답!';
+        numOne = Math.ceil(Math.random() * 9);
+        numTwo = Math.ceil(Math.random() * 9);
+        word.textContent = numOne + " multiple " + numTwo + " = ?";
+        result = numOne * numTwo;
+        input.value = '';
+        input.focus();
+    }
+    else { //틀린경우
+        resultDiv.innerText = '땡!';
+        input.value = '';
+        input.focus();
+    }
+});
--- lecture/lecture.ts
+++ lecture/lecture.ts
@@ -1,3 +1,38 @@
-let numberOne : number = 192;
+let numOne = Math.ceil(Math.random() * 9);
+let numTwo = Math.ceil(Math.random() * 9);
+let result = numOne * numTwo;
 
-console.log(numberOne*9);
+const word = document.createElement('div');
+word.textContent = `${numOne} multiple ${numTwo} = ?`;
+document.body.append(word);
+
+const form = document.createElement('form');
+document.body.append(form);
+
+const input = document.createElement('input');
+form.append(input);
+
+const button = document.createElement('button');
+button.type = 'submit';
+button.textContent = '입력!';
+form.append(button);
+
+const resultDiv = document.createElement('div');
+document.body.append(resultDiv);
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if(result === Number(input.value)){ //정답 맞춘경우
+    resultDiv.innerText = '정답!';
+    numOne = Math.ceil(Math.random() * 9);
+    numTwo = Math.ceil(Math.random() * 9);
+    word.textContent = `${numOne} multiple ${numTwo} = ?`;
+    result = numOne * numTwo;
+    input.value = '';
+    input.focus();
+  } else { //틀린경우
+    resultDiv.innerText = '땡!';
+    input.value = '';
+    input.focus();
+  }
+})
