da7fb372d5c3e4dd6552e97fce9af100f08d561d
kim0612 <roki03@naver.com>
2021-11-16 19:30:15 +0900

    -------------------

---
+++ lecture/example.js
@@ -0,0 +1,4 @@
+var a = 1;
+console.log('hello javascript!');
+console.log('hello typescript!');
+console.log(a);
---
+++ lecture/example.ts
@@ -0,0 +1,5 @@
+const a: number = 1;
+
+console.log('hello javascript!');
+console.log('hello typescript!');
+console.log(a);
